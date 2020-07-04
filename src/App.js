1d81ec8fc7e90634859ceb068f8a399dd1cb7b41
David Obodo <obododavid5@gmail.com>
2020-07-04 10:20:58 +0100

    refactor

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React from 'react';
-// import { Counter } from './features/counter/Counter';
-import TodoWithStatusObject from './components/todo/TodoWithStatus-object';
-import TodoWithStatusArray from './components/todo/TodoWithStatus-array';
+import React from "react";
+import TodoWithStatusObject from "./components/todo/TodoWithStatus-object";
+import TodoWithStatusArray from "./components/todo/TodoWithStatus-array";
 
 const App = () => {
 	return (
